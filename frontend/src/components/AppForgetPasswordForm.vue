25faab773ec37cc120543979aeeacbd9841d3bda
Cyrus <cyrus.kuznetsov@gmail.com>
2023-11-15 02:00:02 +0300

    EQV-38: настройка маски телефона

--- frontend/src/components/AppForgetPasswordForm.vue
+++ frontend/src/components/AppForgetPasswordForm.vue
@@ -1,7 +1,9 @@
 <template>
   <div class="flex flex-wrap font-montserrat">
-    <div class="container mx-auto sm:px-2 mt-5 pt-5 pb-3 mb-5 main-container border border-lightgray rounded-xl shadow-lg hover:shadow-2xl">
-      <div class="flex flex-wrap ">
+    <div
+      class="container mx-auto sm:px-2 mt-5 pt-5 pb-3 mb-5 main-container border border-lightgray rounded-xl shadow-lg hover:shadow-2xl"
+    >
+      <div class="flex flex-wrap">
         <div class="sm:w-4/5 mx-auto">
           <form
             novalidate
@@ -11,9 +13,7 @@
               v-show="step === 1"
               class="step"
             >
-              <h1 class="mb-3 text-center text-base font-bold mt-2">
-                Востановление пароля
-              </h1>
+              <h1 class="mb-3 text-center text-base font-bold mt-2">Востановление пароля</h1>
 
               <h3 class="mb-7 text-center font-weight-light w-11/12 mx-auto text-xs text-darkgray">
                 Введите адрес электронной почты, чтобы сменить пароль
@@ -24,12 +24,14 @@
                   id="exampleInputEmail1"
                   v-model="email"
                   type="email"
-                  :class="errorEmail
-                    ? 'block appearance-none w-4/5 mx-auto py-1 px-2 mb-1 text-sm leading-normal bg-white text-gray-800 border border-borderred rounded-md'
-                    : 'block appearance-none w-4/5 mx-auto py-1 px-2 mb-1 text-sm leading-normal bg-white text-gray-800 border border-borderdarkgray rounded-md' "
+                  :class="
+                    errorEmail
+                      ? 'block appearance-none w-4/5 mx-auto py-1 px-2 mb-1 text-sm leading-normal bg-white text-gray-800 border border-borderred rounded-md'
+                      : 'block appearance-none w-4/5 mx-auto py-1 px-2 mb-1 text-sm leading-normal bg-white text-gray-800 border border-borderdarkgray rounded-md'
+                  "
                   aria-describedby="emailHelp"
                   placeholder="Email"
-                >
+                />
               </div>
 
               <p
@@ -64,14 +66,11 @@
               </div>
             </div>
 
-
             <div
               v-show="step === 2"
               class="step mb-4"
             >
-              <h1 class="mb-3 text-center text-base font-bold mt-2">
-                Востановление пароля
-              </h1>
+              <h1 class="mb-3 text-center text-base font-bold mt-2">Востановление пароля</h1>
 
               <h3 class="mb-7 text-center font-weight-light w-11/12 mx-auto text-xs text-darkgray">
                 Введите новый пароль и подтвердите его
@@ -82,11 +81,13 @@
                   <input
                     v-model="password1"
                     type="password"
-                    :class="!followsCriteria
-                      ? 'block appearance-none mx-auto w-11/12 py-1 px-2 mb-1 text-sm pt-1 pb-1 leading-normal bg-white text-gray-800 border border-red rounded-md'
-                      : 'block appearance-none mx-auto w-11/12 py-1 px-2 mb-1 text-sm pt-1 pb-1 leading-normal bg-white text-gray-800 border border-darkgray rounded-md'"
+                    :class="
+                      !followsCriteria
+                        ? 'block appearance-none mx-auto w-11/12 py-1 px-2 mb-1 text-sm pt-1 pb-1 leading-normal bg-white text-gray-800 border border-red rounded-md'
+                        : 'block appearance-none mx-auto w-11/12 py-1 px-2 mb-1 text-sm pt-1 pb-1 leading-normal bg-white text-gray-800 border border-darkgray rounded-md'
+                    "
                     placeholder="Новый пароль"
-                  >
+                  />
                 </div>
               </div>
 
@@ -106,11 +107,13 @@
                   <input
                     v-model="password2"
                     type="password"
-                    :class="!passwordsAreSame
-                      ? 'block appearance-none mx-auto w-11/12 py-1 px-2 mb-1 text-sm pt-1 pb-1 leading-normal bg-white text-gray-800 border border-borderred rounded-md'
-                      : 'block appearance-none mx-auto w-11/12 py-1 px-2 mb-1 text-sm pt-1 pb-1 leading-normal bg-white text-gray-800 border border-borderdarkgray rounded-md'"
+                    :class="
+                      !passwordsAreSame
+                        ? 'block appearance-none mx-auto w-11/12 py-1 px-2 mb-1 text-sm pt-1 pb-1 leading-normal bg-white text-gray-800 border border-borderred rounded-md'
+                        : 'block appearance-none mx-auto w-11/12 py-1 px-2 mb-1 text-sm pt-1 pb-1 leading-normal bg-white text-gray-800 border border-borderdarkgray rounded-md'
+                    "
                     placeholder="Подтвердите пароль"
-                  >
+                  />
                 </div>
               </div>
 
@@ -137,14 +140,11 @@
               </div>
             </div>
 
-
             <div
               v-show="step === 3"
-              class="step  mb-10"
+              class="step mb-10"
             >
-              <h1 class="mb-3 text-center text-base font-bold mt-2">
-                Пароль изменен!
-              </h1>
+              <h1 class="mb-3 text-center text-base font-bold mt-2">Пароль изменен!</h1>
 
               <h3 class="mb-7 text-center font-weight-light w-11/12 mx-auto text-xs text-darkgray">
                 Теперь вы можете использовать новый пароль, чтобы войти в свой аккаунт
@@ -168,11 +168,11 @@
 </template>
 
 <script lang="ts">
-import validator from 'validator';
+import validator from 'validator'
 export default {
   data() {
     return {
-      step : 1,
+      step: 1,
       errorEmail: false,
       passwordsAreSame: true,
       followsCriteria: true,
@@ -184,8 +184,9 @@ export default {
   },
   methods: {
     newPassword() {
-      this.errorEmail = !validator.isEmail(String(this.email)) || validator.isEmpty(String(this.email))
-      if (!this.errorEmail){
+      this.errorEmail =
+        !validator.isEmail(String(this.email)) || validator.isEmpty(String(this.email))
+      if (!this.errorEmail) {
         this.step = 2
       }
     },
@@ -205,7 +206,7 @@ export default {
 }
 </script>
 <style scoped>
-.main-container{
+.main-container {
   max-width: 300px;
 }
 </style>
