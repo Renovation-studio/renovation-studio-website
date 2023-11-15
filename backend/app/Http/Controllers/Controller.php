97865155275421315468a77fd637e32073ee232d
Cyrus <cyrus.kuznetsov@gmail.com>
2023-11-15 10:28:58 +0300

    EQV-23: настройка SWAGGER и автогенерация доки

--- backend/app/Http/Controllers/Controller.php
+++ backend/app/Http/Controllers/Controller.php
@@ -6,6 +6,20 @@ use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Routing\Controller as BaseController;
 
+/**
+ * @OA\Info(
+ *    title="API for renovation studio project",
+ *    version="1.0.0",
+ * )
+ * @OA\SecurityScheme(
+ *     type="http",
+ *     in="cookie",
+ *     name="session",
+ *     securityScheme="cookieSessionAuth",
+ *     scheme="basic"
+ * )
+
+ */
 class Controller extends BaseController
 {
     use AuthorizesRequests, ValidatesRequests;
