492f69cf43b39801d819874cb1139fe5bcc65609
Andrey Sinko <sinko@intervolga.ru>
2023-12-20 19:04:45 +0300

    RSC-26: Описание нового rest метода в Swagger.

--- laravel/app/Http/Controllers/PhoneController.php
+++ laravel/app/Http/Controllers/PhoneController.php
@@ -7,6 +7,35 @@ use Illuminate\Http\Request;
 
 class PhoneController extends Controller
 {
+    /**
+     * @OA\Post(
+     *     path="/api/phone",
+     *     summary="Эндпоинт для кнопки 'Оставить заяву'",
+     *     description="Добавляет номер телефона и дату заявки в соответствующую таблицу",
+     *     @OA\RequestBody(
+     *         required=true,
+     *         @OA\MediaType(
+     *             mediaType="application/json",
+     *             @OA\Schema(
+     *                 @OA\Property(
+     *                     property="phone",
+     *                     type="string",
+     *                     format="phone",
+     *                     description="Номер телефона в формате +7XXXXXXXXXX"
+     *                 )
+     *             )
+     *         )
+     *     ),
+     *     @OA\Response(
+     *         response=200,
+     *         description="1 в случае успешного добавления, 0 в случае не успешного",
+     *     ),
+     *     @OA\Response(
+     *         response=500,
+     *         description="Не удалось добавить данные",
+     *     )
+     * )
+     */
     public function store(Request $request)
     {
         $phones = Phones::create([
